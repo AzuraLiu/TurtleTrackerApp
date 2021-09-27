286372b7c472d96bc9521883e6a44768339e005d
AzuraLiu <89420619+AzuraLiu@users.noreply.github.com>
2021-09-26 22:43:03 -0400

    Reads data in from file

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,8 +9,22 @@
 # Date:   Fall 2021
 #--------------------------------------------------------------
 
+#Create a variable pointing to the data file
+file_name= './data/raw/sara.txt'
+
+#Create a file object
+file_object = open(file_name, 'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#close the file
+file_object.close()
+
+
 #pretend we read one line of data from file
 lineString='20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+#just select a line from file if read successfully
 
 #split the string into a list of data items
 lineData=lineString.split()
